2747fc25e34b46f1f35305736ccaf24bec0b9eea
Ranabir Chakraborty <60048361+Ridetotheroots@users.noreply.github.com>
2020-10-28 17:17:16 +0530

    Create factorial.java
    how to calculate or find the factorial of a number in Java. Both iterative and recursive version of factorial can be used to calculate factorials but beware of StackOverFlowError if a larger number is passed to the recursive version of the factorial method in Java.

---
+++ codes/factorial.java
@@ -0,0 +1,41 @@
+/**
+ * Simple Java program to find the factorial of a number using recursion and iteration.
+ * Author@ Ranabir Chakraborty
+ */
+public class FactorialInJava{
+
+    public static void main(String args[]) {
+     
+      //finding factorial of a number in Java using recursion - Example
+      System.out.println("factorial of 5 using recursion in Java is: " + factorial(5));
+      System.out.println("factorial of 6 using iteration in Java is: " + fact(6));
+    }
+ 
+ 
+    /*
+     * Java program example to find factorial of a number using recursion
+     * @return factorial of number
+     */
+    public static int factorial(int number){      
+        //base case
+        if(number == 0){
+            return 1;
+        }
+        return number*factorial(number -1); //is this tail-recursion?
+    }
+ 
+    /*
+     * Java program example to calculate factorial using while loop or iteration
+     * @return factorial of number
+     */
+ 
+    public static int fact(int number){
+        int result = 1;
+        while(number != 0){
+            result = result*number;
+            number--;
+        }
+     
+        return result;
+    }
+}
